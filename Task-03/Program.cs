b62a48a575708d7ec08bb47da1e891475e6c341d
ir-shadd0812 <irshad.aalam0812@gmail.com>
2025-11-10 09:53:10 +0545

    Add Task-03: AgeChecker program

---
+++ Task-03/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        // Loop through the ages if else if 
+        if (age < 13)
+            Console.WriteLine("Child");
+        else if (age >= 13 && age <= 19)
+            Console.WriteLine("Teenager");
+        else
+            Console.WriteLine("Adult");
+
+        // Using switch expression 
+        string category = age switch
+        {
+            < 13 => "Child",
+            >= 13 and <= 19 => "Teenager",
+            _ => "Adult"
+        };
+
+        Console.WriteLine("Using switch: " + category);
+    }
+}
